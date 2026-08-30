5b4f59bea4d4ca1683ae4f619d6fcf74263d4cc3
Darko Ezekiel Owusu <ezekieldarko2026@gmail.com>
2026-08-30 09:43:13 +0000

    Update API key retrieval in app.py
    Replaced the API key retrieval method with a hardcoded value.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from google.genai import types
 
 # Load API key from .env file
 load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
+api_key = os.getenv("AQ.Ab8RN6J68094yU-FCkk2ciIFLDQnWAZjJY0kZi2phgnZ7RF4yA")
 
 # Set up the Google GenAI Client
 if api_key:
